9bf5ab71a5a06d22ac3c142725fc263f019db8cf
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2023-12-05 14:48:57 +0600

    background: linear-gradient(253deg, #FBF9FA -6.16%, #FCFCFC 48.58%, #FFF5F9 86.54%);

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,8 @@ import('tailwindcss').Config
 module.exports = {
   content: [],
   theme: {
-    container: {
-      screens: {
-          '2xl': '1280px',
-      }
-  }
+    
+  
   },
   plugins: [],
 }
